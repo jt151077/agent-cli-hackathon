f0f7a788675b2cc8bbc20661682c8195ea980b8c
jt151077 <jt151077@gmail.com>
2026-05-06 09:56:07 +0000

    first commit

---
+++ app/agent.py
@@ -0,0 +1,47 @@
+# ruff: noqa
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.adk.agents import Agent
+from google.adk.apps import App
+from google.adk.models import Gemini
+from google.adk.tools import google_search
+from google.genai import types
+
+import os
+import google.auth
+
+_, project_id = google.auth.default()
+os.environ["GOOGLE_CLOUD_PROJECT"] = project_id
+os.environ["GOOGLE_CLOUD_LOCATION"] = "global"
+os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "True"
+
+
+
+root_agent = Agent(
+    name="root_agent",
+    model=Gemini(
+        model="gemini-flash-latest",
+        retry_options=types.HttpRetryOptions(attempts=3),
+    ),
+    instruction="""
+      You are a Google search expert. Help answer the user questions to your best ability by leverage `google_search`.
+    """,
+    tools=[google_search],
+)
+
+app = App(
+    root_agent=root_agent,
+    name="app",
+)
